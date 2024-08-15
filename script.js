a999ad9e2511fd83cf97b665380b67eba13c1fb5
yasmin <128601449+yasminbastos@users.noreply.github.com>
2024-08-15 10:02:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script Carregado");
+function calcularAnos();
+console.log("Botão de Calcular clicando");
+let popAInput = document.getElementById("popA").value.trim();
+let taxaAInput = document.getElementById("taxaA").value.trim();
+let popBInput = document.getElementById("popB").value.trim();
+let taxaBInput = document.getElementById("taxaB").value.trim();
